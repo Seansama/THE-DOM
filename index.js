efc8204811fd98cf0ace50d89fe306591da4e24f
Shaun <seansama@pop-os.localdomain>
2022-12-08 15:26:00 +0300

    querySelector and querySelectorAll

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-let titles = document.getElementsByClassName(`title`);
 /*
+let titles = document.getElementsByClassName(`title`);
+
 console.log(Array.isArray(titles))
 console.log(Array.isArray(Array.from(titles)))
-*/
+
 Array.from(titles).forEach(function (item) {
     console.log(item)
+})
+*/
+const wmf = document.querySelector(`#book-list li:nth-child(2) .name`)
+//console.log(wmf)
+let books = document.querySelector(`book-list li .name`)
+//console.log(`books`)
+
+books = document.querySelectorAll(`#book-list li .name`)
+console.log(books)
+Array.from(books).forEach(function (book) {
+    console.log(book)
 })
