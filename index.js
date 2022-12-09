02c3a9f1da6c338c843340347a062ecd04f4263f
Shaun <seansama@pop-os.localdomain>
2022-12-09 11:54:21 +0300

    Adding content to HTML

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ Array.from(titles).forEach(function (item) {
     console.log(item)
 })
 */
+
+/*
 const wmf = document.querySelector(`#book-list li:nth-child(2) .name`)
 //console.log(wmf)
 let books = document.querySelector(`book-list li .name`)
@@ -18,3 +20,15 @@ console.log(books)
 Array.from(books).forEach(function (book) {
     console.log(book)
 })
+ */
+let books = document.querySelector(`book-list li .name`)
+//console.log(`books`)
+
+books = document.querySelectorAll(`#book-list li .name`)
+Array.from(books).forEach(function (book){
+    book.textContent += `(book title)`;
+
+})
+const bookList = document.querySelector(`#book-list`);
+// bookList.innerHTML = `<h2>Books and more Books...</h2>`
+bookList.innerHTML += `<p>This is how you add HTML</p>`
