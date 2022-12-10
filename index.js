1e68466b45846464b2f931ffe1ce0cec8a8d1315
Shaun <seansama@pop-os.localdomain>
2022-12-11 00:20:51 +0300

    DOMcontentLoaded. End of lesson

--- index.js
+++ index.js
@@ -1,93 +1,96 @@
+//DOMcontentLoaded event
+document.addEventListener(`DOMContentLoaded`, () => {
+    //PreventDefault event
+    const link = document.querySelector(`#page-banner a`)
 
-//PreventDefault event
-const link = document.querySelector(`#page-banner a`)
+    link.addEventListener(`click`, function (e) {
+        e.preventDefault();
+        console.log(`navigation to `, e.target.textContent, `was prevented`)
 
-link.addEventListener(`click`, function (e) {
-    e.preventDefault();
-    console.log(`navigation to `, e.target.textContent, `was prevented`)
-    
-})
+    })
 
 //Delete button functionality using event bubbling
-const list = document.querySelector(`#book-list ul`)
-list.addEventListener(`click`, function (e) {
-    if(e.target.className === `delete`){
-        const  li = e.target.parentElement;
-        list.removeChild(li)
-    }
+    const list = document.querySelector(`#book-list ul`)
+    list.addEventListener(`click`, function (e) {
+        if(e.target.className === `delete`){
+            const  li = e.target.parentElement;
+            list.removeChild(li)
+        }
 
-})
+    })
 
 //interacting with forms
-const addForm = document.forms[`add-book`]
-addForm.addEventListener(`submit`, function (e) {
-    e.preventDefault();
-    const value = addForm.querySelector(`input[type='text']`).value
-    //console.log(value)
+    const addForm = document.forms[`add-book`]
+    addForm.addEventListener(`submit`, function (e) {
+        e.preventDefault();
+        const value = addForm.querySelector(`input[type='text']`).value
+        //console.log(value)
 
-    //create Elements
-    const li =document.createElement(`li`);
-    const bookName = document.createElement(`span`)
-    const deleteBtn = document.createElement(`span`)
+        //create Elements
+        const li =document.createElement(`li`);
+        const bookName = document.createElement(`span`)
+        const deleteBtn = document.createElement(`span`)
 
-    deleteBtn.textContent = 'delete';
-    bookName.textContent = value;
+        deleteBtn.textContent = 'delete';
+        bookName.textContent = value;
 
-    //add classes
-    bookName.classList.add('name')
-    deleteBtn.classList.add('delete')
+        //add classes
+        bookName.classList.add('name')
+        deleteBtn.classList.add('delete')
 
-    //append to DOM
-    li.appendChild(bookName)
-    li.appendChild(deleteBtn)
-    list.appendChild(li)
+        //append to DOM
+        li.appendChild(bookName)
+        li.appendChild(deleteBtn)
+        list.appendChild(li)
 
-})
+    })
 
 //Hide checkbox and change events
-const hideBox = document.querySelector(`#Hide`)
-hideBox.addEventListener(`change`, function () {
-    if(hideBox.checked){
-        list.style.display = `none`;
-    }
-    else{
-        list.style.display = `initial`;
-    }
-
-})
-
-//Filter searched books
-const searchBar = document.forms[`search-books`].querySelector(`input`)
-searchBar.addEventListener(`keyup`, function (e) {
-    const term = e.target.value.toLowerCase()
-    const books = list.getElementsByTagName(`li`)
-    Array.from(books).forEach(function (book) {
-        const title = book.firstElementChild.textContent;
-        if(title.toLowerCase().indexOf(term) !== -1){
-book.style.display = 'block';
+    const hideBox = document.querySelector(`#Hide`)
+    hideBox.addEventListener(`change`, function () {
+        if(hideBox.checked){
+            list.style.display = `none`;
         }
         else{
-            book.style.display = 'none'
+            list.style.display = `initial`;
         }
-        
+
     })
 
-})
-
-//Tabbed content
-const tabs = document.querySelector(`.tabs`)
-const panels = document.querySelector(`.panel`)
-tabs.addEventListener(`click`, (e) => {
-    if(e.target.tagName === 'LI'){
-        const targetPanel = document.querySelector(e.target.dataset.target)
-        panels.forEach((panel) => {
-            if (panel === targetPanel){
-                panel.classList.add(`active`)
+//Filter searched books
+    const searchBar = document.forms[`search-books`].querySelector(`input`)
+    searchBar.addEventListener(`keyup`, function (e) {
+        const term = e.target.value.toLowerCase()
+        const books = list.getElementsByTagName(`li`)
+        Array.from(books).forEach(function (book) {
+            const title = book.firstElementChild.textContent;
+            if(title.toLowerCase().indexOf(term) !== -1){
+                book.style.display = 'block';
             }
             else{
-                panel.classList.remove(`active`)
+                book.style.display = 'none'
             }
+
         })
-    }
+
     })
 
+//Tabbed content
+    const tabs = document.querySelector(`.tabs`)
+    const panels = document.querySelector(`.panel`)
+    tabs.addEventListener(`click`, (e) => {
+        if(e.target.tagName === 'LI'){
+            const targetPanel = document.querySelector(e.target.dataset.target)
+            panels.forEach((panel) => {
+                if (panel === targetPanel){
+                    panel.classList.add(`active`)
+                }
+                else{
+                    panel.classList.remove(`active`)
+                }
+            })
+        }
+    })
+
+
+})
